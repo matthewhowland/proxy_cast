c8f2d0d614f09ce863798eca7ac406fd06056c8d
Matthew Howland <howlandm@gmail.com>
2016-07-19 19:52:25 -0400

    added status codes to the return of the hourly forecast

--- index.js
+++ index.js
@@ -22,10 +22,13 @@ server.get('/forecast/hourly/:lat,:lon', function(req, res){
       longitude: response.data.longitude,
       hourly: response.data.hourly,
     };
-    res.json(resObj);
+    res.status(200).json(resObj);
   })
   .catch(function(error){
-  res.send('bad');
+  res.status(500).send({
+    msg: 'error'
+   })
+  });
 });
 
 });
