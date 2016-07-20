793bb57f4bc0ddf3e287e1d8ad248a5ebd73b587
Matthew Howland <howlandm@gmail.com>
2016-07-20 19:08:44 -0400

    implemented get routes

--- index.js
+++ index.js
@@ -44,26 +44,57 @@ server.get('/forecast/minutely/:lat,:lon', function(req, res){
   .catch(function(error){
   res.status(500).send({
     msg: 'error'
-   })
+  })
   });
 });
 
 server.get('/forecast/daily/:lat,:lon', function(req, res){
   $http.get(baseUrl + apiKey + '/'+req.params.lat+','+req.params.lon)
   .then(function(response){
+    var overSummary = response.data.daily.summary;
+    var overIcon = response.data.daily.icon;
+    var dailyData = response.data.daily.data;
+    var dailyArr = [];
+    for(var i = 0; i< dailyData.length; i +=1){
+      var o = {
+        icon: dailyData[i].icon,
+        tempMax: dailyData[i].temperatureMax,
+        tempMin: dailyData[i].temperatureMin,
+        humidity: dailyData[i].humidity,
+        precipProb: dailyData[i].precipProbability
+      };
+      dailyArr.push(o);
+    }
+
     var resObj = {
       latitude: response.data.latitude,
       longitude: response.data.longitude,
-      daily: response.data.daily,
+      summary: overSummary,
+      icon: overIcon,
+      daily: dailyArr
     };
+
     res.status(200).json(resObj);
   })
-  .catch(function(error){
-    res.status(500).send({
-      msg: 'error'
-  })
+  .catch(function(err){
+    res.status(500).json({
+      msg: err
+    });
+  });
 });
-});
+//     var resObj = {
+//       latitude: response.data.latitude,
+//       longitude: response.data.longitude,
+//       daily: response.data.daily,
+//     };
+//     res.status(200).json(resObj);
+//   })
+//   .catch(function(error){
+//     res.status(500).send({
+//       msg: 'error'
+//   })
+// });
+// });
 
 // server.get('/forecast/daily/:lat,:lon', function(req, res){
 //   $http.get(baseUrl + apiKey + '/'+req.params.lat+','+req.params.lon)
